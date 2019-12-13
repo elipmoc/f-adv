e6d0c49ba1e0519ee413c6987b667894537b236d
elipmoc <relipmoc101@gmail.com>
2019-12-14 05:14:48 +0900

    first commit

---
+++ FADVForm/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using FADVLib;
+
+namespace FADVForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(testRoslyn.build("hoge.exe","hoge"));
+        }
+    }
+}
---
+++ FADVLib/Library1.fs
@@ -0,0 +1,96 @@
+﻿namespace FADVLib
+
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis
+
+module testRoslyn=
+
+    //using System;のコード生成
+    let usingList=
+        SyntaxList[|SyntaxFactory.UsingDirective(SyntaxFactory.ParseName "System")|]
+
+    (*
+    static void Main(){
+        Console.WriteLine("HelloWorld");
+    }
+    のコード生成
+    *)
+    let mainMethod=
+        SyntaxFactory.MethodDeclaration(
+            SyntaxList(),
+            SyntaxFactory.TokenList(SyntaxFactory.Token SyntaxKind.StaticKeyword),
+            SyntaxFactory.PredefinedType(SyntaxFactory.Token SyntaxKind.VoidKeyword), 
+            null,
+            SyntaxFactory.ParseToken "Main",
+            SyntaxFactory.TypeParameterList(),
+            SyntaxFactory.ParameterList(),
+            SyntaxList(),
+            SyntaxFactory.Block [|
+                SyntaxFactory.ExpressionStatement(
+                    SyntaxFactory.InvocationExpression(
+                        SyntaxFactory.ParseExpression "Console.WriteLine",
+                        SyntaxFactory.ArgumentList(
+                            SyntaxFactory.SeparatedList [| 
+                                SyntaxFactory.Argument(SyntaxFactory.ParseExpression "\"HelloWorld\"")
+                            |]
+                        )
+                    )
+                )
+                :>Syntax.StatementSyntax
+            |],
+            SyntaxFactory.Token SyntaxKind.SemicolonToken
+        )
+
+    //class Programのコード生成
+    let memberDeclarationList:SyntaxList<Syntax.MemberDeclarationSyntax> =
+        (
+            SyntaxFactory.ClassDeclaration(
+                SyntaxList(),
+                SyntaxTokenList(),
+                SyntaxFactory.ParseToken "Program",
+                SyntaxFactory.TypeParameterList(),
+                SyntaxFactory.BaseList(),
+                SyntaxList (),
+                SyntaxList (mainMethod:>Syntax.MemberDeclarationSyntax)
+            )
+            :>Syntax.MemberDeclarationSyntax
+        )
+        |> SyntaxList
+
+    //コンパイルできる最小単位の生成
+    let compilationUnit:Syntax.CompilationUnitSyntax =
+        SyntaxFactory.CompilationUnit(SyntaxList(),usingList,SyntaxList(),memberDeclarationList)
+
+    //コンパイルできる最小単位の集合
+    let syntaxTreeList :SyntaxTree[]=
+        [|
+            CSharpSyntaxTree.Create compilationUnit
+        |]
+
+    //コンパイル結果のエラーメッセージ整形
+    let resultMessage :(seq<Diagnostic>->string) =
+        Seq.map (fun diagnostic->diagnostic.GetMessage())
+        >> Seq.fold (fun acc x->acc+x) ""
+
+    //コンパイルに必要なアセンブリ一覧
+    let addAssembly :MetadataReference[]=
+        [|
+            MetadataReference.CreateFromFile  typeof<System.Object>.Assembly.Location :>MetadataReference
+        |]
+        
+    //コンパイルインスタンス生成
+    let createCompilation (assemblyName:string) :CSharpCompilation =
+        CSharpCompilation.Create(assemblyName,syntaxTreeList)
+            .AddReferences
+                addAssembly
+
+    //コンパイルインスタンスをEmitでexe出力
+    let emit (fileName:string) (compilation:CSharpCompilation)  : seq<Diagnostic> =
+        compilation.Emit(fileName).Diagnostics
+        :>seq<Diagnostic>
+
+    //全てのワークフローを開始する
+    let build (fileName:string) (assemblyName:string):string=
+        createCompilation assemblyName
+        |>emit fileName
+        |>resultMessage
