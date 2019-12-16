eb44fda1cb3bbffd315d5359e5dd1862d1a88924
elipmoc <relipmoc101@gmail.com>
2019-12-17 02:07:06 +0900

    簡単なASTの定義

---
+++ FADVLib/AST/ScriptAst.fs
@@ -0,0 +1,19 @@
+﻿module ScriptAst
+
+type ActionAst=
+    |Exit
+
+type MessageAst= 
+    {
+        SpeakerText:string
+        MessageText:string
+    }
+
+type StatementAst=
+    | ActionAst of ActionAst
+    | MessageAst of MessageAst
+
+type ScriptAst=
+    {
+        StatementAstList:StatementAst[]
+    }
