cff3e69d6b37b0e1b4f26c25d14178769947bb33
elipmoc <relipmoc101@gmail.com>
2019-12-20 01:33:53 +0900

    projectInfoのjson変換に関するテスト

---
+++ FADVLibTest/ProjectServices/ProjectInfoJsonTest.fs
@@ -0,0 +1,12 @@
+﻿namespace FADVLibTest.ProjectInfoJsonTest
+
+open NUnit.Framework
+open FADVLib.ProjectServices.ProjectInfoRepository
+open FADVLib.ProjectServices.ProjectInfo
+
+[<TestFixture>]
+type  ProjectInfoJsonTest() =
+
+    [<Test>]
+    member this.toJsonPassing() =
+        Assert.AreEqual("{\"name\":\"hogefuga\"}",toJsonFromProjectInfo {Name="hogefuga"})
