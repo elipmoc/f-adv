843aed3722da4534c130d76d5780a9a746897e4c
elipmoc <relipmoc101@gmail.com>
2019-12-19 14:31:18 +0900

    projectInfoを保存するプログラムを仮で作成

---
+++ FADVLib/ProjectServices/ProjectInfoRepository.fs
@@ -0,0 +1,10 @@
+﻿module FADVLib.ProjectService.ProjectInfoRepository
+open System.IO
+open FADVLib.ProjectServices.ProjectInfo
+
+//プロジェクトを保存する
+let saveProjectInfo (folderPath:string)  (projectInfo:ProjectInfo):unit = 
+     let sw=new StreamWriter(folderPath+"/"+projectInfo.Name+".json" |> File.Create)
+     sw.WriteLine("{\"name\":\""+projectInfo.Name+"\"}")
+     sw.Close()
+     ()
