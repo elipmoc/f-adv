9bdf65a9e2b0813b50af204b0ae627234d748025
elipmoc <relipmoc101@gmail.com>
2019-12-19 14:49:27 +0900

    フォームのタイトル名変更

--- FADVForm/Form1.Designer.cs
+++ FADVForm/Form1.Designer.cs
@@ -105,7 +105,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FADV";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
