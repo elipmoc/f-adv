4eb6df8ea49a42f0d0525eea373162de13986aa6
elipmoc <relipmoc101@gmail.com>
2019-12-20 01:19:44 +0900

    testプロジェクトを作成

---
+++ FADVLibTest/Program.fs
@@ -0,0 +1,13 @@
+﻿namespace FADVLibTest
+
+open NUnit.Framework
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member this.TestMethodPassing() =
+        Assert.True(true)
+
+    [<Test>]
+     member this.FailEveryTime() = Assert.True(false)
